882dd024e42885ed441d5de89c83409f7c8e2426
kateBy <nonameisiam@yandex.ru>
2016-06-28 22:46:00 +0300

    Файл патча asm создаётся и модифицируется скриптом. Косметические правки кода

--- main.py
+++ main.py
@@ -220,16 +220,16 @@ for menuitem in main_menu:
 print("Патчится функция выравнивания строк")
 offset = 0x8778C8C #FIXME найти способ нахождения функции автоматически
 binFile = '/tmp/str_resize_path.bin'
-os.system('bash ./asm/get_lib_addr.sh ' + binFile)
+os.system('bash ./asm/str_resize_patch.sh ' + binFile)
 
 CALL_SIZE = 5
 if exists(binFile):
     call = opcodes.make_call(offset + CALL_SIZE, CURSOR + NEW_BASE_ADDR)
     e_df.seek(offset - OLD_BASE_ADDR)
     e_df.write(call) #Создаем CALL-перехват управления на новую функцию
-    e_df.seek(CURSOR+NEW_OFFSET)
+    e_df.seek(CURSOR + NEW_OFFSET)
     asm_patch = open(binFile, 'rb').read()
-    e_df.write(asm_patch) #Записываем результат работы FASM в файл
+    e_df.write(asm_patch) #Патчим файл результатом FASM
     CURSOR += len(asm_patch) + 1
     os.remove(binFile)
 else:
