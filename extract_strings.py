74109988f2e97a176423ab81229d0fd325ee9c6d
kateBy <nonameisiam@yandex.ru>
2016-07-01 09:18:31 +0300

    Косметическая правка кода extract_strings.py

--- extract_strings.py
+++ extract_strings.py
@@ -8,6 +8,8 @@ The finest place,0 и place,0 будут совмещены в одно мест
 chars = set(i for i in range(32, 127))
 nums = b'1234567890'
 
+
+
 """Проверяем найденую строку на нечитаемые символы"""
 def check_forbidden(byte_string):
     if len(byte_string) < 2:
@@ -43,6 +45,8 @@ def find_gemini(words, translated):
 
     return result
 
+
+
 """Проверяем, имеется ли в буфере ссылка с таким адресом"""
 def check_founded_gemini(gemini, buf):
     result = {}
@@ -60,22 +64,9 @@ def check_founded_gemini(gemini, buf):
         if (ind % 100) == 0:
             print("%.2f" % (ind / max_ind * 100), "%")
 
-
-    SPLIT_SYMBOL = "<*|*>"
-
-
-    cache_file = open('gemini_cache.txt', 'wt')
-    for w in result:
-        line = str(w) + SPLIT_SYMBOL + str(result[w]) + "\n"
-        cache_file.write(line)
-
-    cache_file.close()
-        
-
     return result
 
             
-
 
 """Извлекает все похожее на строки из секции .rodata"""
 def extract_strings(fn):
@@ -101,28 +92,14 @@ def extract_strings(fn):
                 checked = check_forbidden(rodata[index:next_zero])
                 if checked != None:
                     words[checked] = index + rodata_vaddr
-                    
-            
                 index = next_zero
             
-
         index += 1
     return words
 
-'''Загрузка строк перевода из файла trans.txt'''
-def load_trans_txt(fn):
-    trans = open(fn, 'rt', encoding="cp1251")
-    words = {}
-    for line in trans:
-        splitted = line.split("|")
-        if len(splitted) < 4:
-            continue
-        else:
-            words[splitted[1]] = splitted[2]
 
-    return words
 
-'''Загрузка перевода из файла .mo с помощью библиотеки polib'''
+'''Загрузка перевода из файла .po с помощью библиотеки polib'''
 def load_trans_po(fn):
     import polib
     result = {}
@@ -141,6 +118,8 @@ def load_trans_po(fn):
             result[i.msgid] = i.msgstr
 
     return result
+
+
 
 """Создаёт секцию с новыми строками и возвращает словарь,
 содержащий английскую версию строки и индекс ее в новой секции.
@@ -166,6 +145,8 @@ def make_dat_file(fn, trans, size = 0x100000):
 
     return result
 
+
+
 """Разбивает словарь на несколько словарей,
 нужно для запуска нескольких потоков замены индексов"""
 def split_dictionary(some_dict, out_count):
@@ -179,3 +160,5 @@ def split_dictionary(some_dict, out_count):
             cursor = 0
 
     return result
+
+
