1584af6f7748ee63c3b34fd2813485ffbe98025e
kateBy <nonameisiam@yandex.ru>
2016-07-02 18:13:26 +0300

    Изменен патч перед вызовом функций

--- asm/str_len.asm
+++ asm/str_len.asm
@@ -1,22 +1,18 @@
-use32
+use64
 
-RU_OFFSET = 0x9f7a000 ;%_RU_OFFSET_% ;Смещение секции .rus
+RU_OFFSET = 0x222b000 ;%_RU_OFFSET_% ;Смещение секции .rus
 RU_SIZE   = 0x100000  ;%_RU_SIZE_%   ;Размер секции .rus
 RU_END    = RU_OFFSET + RU_SIZE - 1
 
-push    edi            ;Сохраняем edi
+;esi - сдвиг на строку
+;edx - длина строки
 
-mov	edi, [esp+12]  ;Передаём указатель на первый байт строки
-
-cmp     edi, RU_OFFSET ;Проверяем, находится ли строка в секции .rus
+cmp     esi, RU_OFFSET ;Проверяем, находится ли строка в секции .rus
 jb      NOT_RUS        ;Если сдвиг не в секции, то править длину не нужно
-cmp     edi, RU_END    ;Проверяем, находится ли строка в секции .rus
+cmp     esi, RU_END    ;Проверяем, находится ли строка в секции .rus
 ja      NOT_RUS
     
-    mov edi, [edi-4]   ;Загружаем длину строки в edi
-    mov [esp+16], edi  ;Передаем длину в стёк
-
+    mov dword edx, [esi-4]   ;Загружаем длину строки в edx
 
 NOT_RUS:
-
-pop	edi            ;Восстанавливаем edi
+nop
