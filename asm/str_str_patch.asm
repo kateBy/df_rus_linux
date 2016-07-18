20855b7b5e2b93fb21bf466fd1785f1804260bdd
kateBy <nonameisiam@yandex.ru>
2016-07-18 23:32:07 +0300

    Пункты главного меню теперь выравниваются при любой ширине экрана

--- asm/str_str_patch.asm
+++ asm/str_str_patch.asm
@@ -36,56 +36,73 @@ jne @NO
 jmp qword [qword FUNC_ADDR]       ;Переход к функции
 
 @CONTINUE:
-strcopy sCONTINUE, lCONTINUE, 8, 0x28-lCONTINUE/2
+strcopy sCONTINUE, lCONTINUE, zCONTINUE, 8
 jmp @NO
 
 @START:
-strcopy sSTART, lSTART, 8, 0x2a-lSTART/2
+strcopy sSTART, lSTART, zSTART, 8
 jmp @NO
 
 @QUIT:
-mov rax, qword [sQUIT]
-mov [rsi], rax
-mov rax, 0x29-lQUIT/2
-mov [qword GPS], rax
+strcopy sQUIT, lQUIT, zQUIT, 1
 jmp @NO
 
 @CREATE: 
-strcopy sCREATE, lCREATE, 8, 0x2b-lCREATE/2
+strcopy sCREATE, lCREATE, zCREATE, 8
 jmp @NO
 
 @ABOUT:
-mov rax, qword [sABOUT]
-mov [rsi], rax
+strcopy sABOUT, lABOUT, zABOUT, 1
 jmp @NO
 
 @DESIGN:
-strcopy sDESIGN, lDESIGN, 4, 0x29-lDESIGN/2
+strcopy sDESIGN, lDESIGN, zDESIGN, 8
 jmp @NO
 
 @OBJECT:
-strcopy sOBJECT, lOBJECT, 4, 0x2a-lOBJECT/2
+strcopy sOBJECT, lOBJECT, zOBJECT, 8
 jmp @NO
 
 @HISTOR:
-strcopy sHISTOR, lHISTOR, 4, -1
+strcopy sHISTOR, lHISTOR, lHISTOR, 4
 jmp @NO
 
 ;Переведенные слова в cp1251
-sCONTINUE  db 207, 240, 238, 228, 238, 235, 230, 232, 242, 252, 32, 200, 227, 240, 243, 0; "Продолжить Игру",0
-lCONTINUE = $-sCONTINUE
-sSTART     db 205, 224, 247, 224, 242, 252, 32, 200, 227, 240, 243, 5 dup(0)             ; "Начать Игру",0
+sCONTINUE  db 207, 240, 238, 228, 238, 235, 230, 232, 242, 252, 32, 200, 227, 240, 243; "Продолжить Игру",0
+lCONTINUE = $-sCONTINUE ; Длина строки без дополнительных нулей
+db 0
+zCONTINUE = $-sCONTINUE ; Полная длина строки с нулями
+
+sSTART     db 205, 224, 247, 224, 242, 252, 32, 200, 227, 240, 243                    ; "Начать Игру",0
 lSTART  = $-sSTART
-sQUIT      db 194, 251, 245, 238, 228, 3 dup(0)                                          ; "Выход",0
+db 5 dup(0)
+zSTART = $-sSTART
+
+sQUIT      db 194, 251, 245, 238, 228                                                 ; "Выход",0
 lQUIT   = $-sQUIT
-sCREATE    db 209, 238, 231, 228, 224, 242, 252, 32, 205, 238, 226, 251, 233, 32, 204, 232, 240, 33, 6 dup(0); Создать Новый Мир!
+db 3 dup(0)
+zQUIT   = $-sQUIT
+
+sCREATE    db 209, 238, 231, 228, 224, 242, 252, 32, 205, 238, 226, 251, 233, 32, 204, 232, 240, 33; Создать Новый Мир!
 lCREATE = $-sCREATE
-sABOUT     db 206, 225, 32, 200, 227, 240, 229, 0 ; Об Игре
+db 6 dup(0)
+zCREATE = $-sCREATE
+
+sABOUT     db 206, 225, 32, 200, 227, 240, 229 ; Об Игре
 lABOUT  = $-sABOUT
-sDESIGN    db 209,238,231,228,224,242,252,32,204,232,240,32,241,32,208,224,241,248,232,240,229,237,237,251,236,232,32,207,224,240,224,236,229,242,240,224,236,232,0,0
+db 0
+zABOUT = $-sABOUT
+
+sDESIGN    db 209,238,231,228,224,242,252,32,204,232,240,32,241,32,208,224,241,248,232,240,229,237,237,251,236,232,32,207,224,240,224,236,229,242,240,224,236,232
 lDESIGN = $-sDESIGN
-sOBJECT    db 192,240,229,237,224,32,210,229,241,242,232,240,238,226,224,237,232,255,32,206,225,250,229,234,242,238,226, 5 dup(0)
+db 0,0
+zDESIGN = $-sDESIGN
+
+sOBJECT    db 192,240,229,237,224,32,210,229,241,242,232,240,238,226,224,237,232,255,32,206,225,250,229,234,242,238,226
 lOBJECT = $-sOBJECT
+db 5 dup(0)
+zOBJECT = $-sOBJECT
+
 sHISTOR    db 32, 32, 32, 200, 241, 242, 238, 240, 232, 232, 32, 238
 lHISTOR = $-sHISTOR
 
