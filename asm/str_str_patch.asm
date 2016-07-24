741c531bbf39b65ba89d77021b96d558294413d2
kateBy <nonameisiam@yandex.ru>
2016-07-25 00:16:34 +0300

    Правка патча строк главного меню

--- asm/str_str_patch.asm
+++ asm/str_str_patch.asm
@@ -36,74 +36,60 @@ jne @NO
 jmp qword [qword FUNC_ADDR]       ;Переход к функции
 
 @CONTINUE:
-strcopy sCONTINUE, lCONTINUE, zCONTINUE, 8
+strcopy sCONTINUE, lCONTINUE, 8
 jmp @NO
 
 @START:
-strcopy sSTART, lSTART, zSTART, 8
+strcopy sSTART, lSTART, 8
 jmp @NO
 
 @QUIT:
-strcopy sQUIT, lQUIT, zQUIT, 8
+strcopy sQUIT, lQUIT, 8
 jmp @NO
 
 @CREATE: 
-strcopy sCREATE, lCREATE, zCREATE, 8
+strcopy sCREATE, lCREATE, 8
 jmp @NO
 
 @ABOUT:
-strcopy sABOUT, lABOUT, zABOUT, 8
+strcopy sABOUT, lABOUT, 8
 jmp @NO
 
 @DESIGN:
-strcopy sDESIGN, lDESIGN, zDESIGN, 8
+strcopy sDESIGN, lDESIGN, 8
 jmp @NO
 
 @OBJECT:
-strcopy sOBJECT, lOBJECT, zOBJECT, 8
+strcopy sOBJECT, lOBJECT, 8
 jmp @NO
 
 @HISTOR:
-strcopy sHISTOR, lHISTOR, lHISTOR, 4
+strcopy sHISTOR, lHISTOR, 1
 jmp @NO
 
 ;Переведенные слова в cp1251
-sCONTINUE  db 207, 240, 238, 228, 238, 235, 230, 232, 242, 252, 32, 200, 227, 240, 243; "Продолжить Игру",0
+sCONTINUE  db 207, 240, 238, 228, 238, 235, 230, 232, 242, 252, 32, 200, 227, 240, 243,0; "Продолжить Игру",0
 lCONTINUE = $-sCONTINUE ; Длина строки без дополнительных нулей
-make_align lCONTINUE, 8 ; Добавляем нулей после строки, чтобы длина была кратна 8
-zCONTINUE = $-sCONTINUE ; Полная длина строки с нулями
 
-sSTART     db 205, 224, 247, 224, 242, 252, 32, 200, 227, 240, 243                    ; "Начать Игру",0
-lSTART  = $-sSTART
-make_align lSTART, 8
-zSTART = $-sSTART
+sSTART     db 205, 224, 247, 224, 242, 252, 32, 200, 227, 240, 243,0                    ; "Начать Игру",0
+lSTART = $-sSTART
 
-sQUIT      db 194, 251, 245, 238, 228                                                 ; "Выход",0
-lQUIT   = $-sQUIT
-make_align lQUIT, 8
-zQUIT   = $-sQUIT
+sQUIT      db 194, 251, 245, 238, 228,0                                                ; "Выход",0
+lQUIT = $-sQUIT
 
-sCREATE    db 209, 238, 231, 228, 224, 242, 252, 32, 205, 238, 226, 251, 233, 32, 204, 232, 240, 33; Создать Новый Мир!
-lCREATE = $-sCREATE
-make_align lCREATE, 8
-zCREATE = $-sCREATE
+sCREATE    db 209,238,231,228,224,242,252,32,205,238,226,251,233,32,204,232,240,33,0; Создать Новый Мир!
+lCREATE = $-sCREATE-1
 
-sABOUT     db 206, 225, 32, 200, 227, 240, 229 ; Об Игре
-lABOUT  = $-sABOUT
-make_align lABOUT, 8
-zABOUT = $-sABOUT
+sABOUT     db 206,225,32,200,227,240,229,0 ; Об Игре
+lABOUT = $-sABOUT
 
-sDESIGN    db 209,238,231,228,224,242,252,32,204,232,240,32,241,32,208,224,241,248,232,240,229,237,237,251,236,232,32,207,224,240,224,236,229,242,240,224,236,232
+sDESIGN    db 209,238,231,228,224,242,252,32,204,232,240,32,241,32,208,224,241,248,232,240,229,237,237,251,236,232,32,207,224,240,224,236,229,242,240,224,236,232,0
 lDESIGN = $-sDESIGN
-make_align lDESIGN, 8
-zDESIGN = $-sDESIGN
 
-sOBJECT    db 192,240,229,237,224,32,210,229,241,242,232,240,238,226,224,237,232,255,32,206,225,250,229,234,242,238,226
+sOBJECT    db 192,240,229,237,224,32,210,229,241,242,232,240,238,226,224,237,232,255,32,206,225,250,229,234,242,238,226,0
 lOBJECT = $-sOBJECT
-make_align lOBJECT, 8
-zOBJECT = $-sOBJECT
 
-sHISTOR    db 32, 32, 32, 200, 241, 242, 238, 240, 232, 232, 32, 238
+sHISTOR    db 32, 32, 32, 32, 200, 241, 242, 238, 240, 232, 232, 32, 238
 lHISTOR = $-sHISTOR
 
 ;Полные слова для поиска соответствия, но только по первым 8-ми байтам
